2da923d471c8eeb0af1f351430d330e5360fff53
Francesco Ciulla <francescociullaroma@gmail.com>
2018-05-17 02:49:02 +0200

    added simple endpoint

---
+++ src/main/java/com/francescoXX/springboothttpsexample/HelloResource.java
@@ -0,0 +1,20 @@
+package com.francescoXX.springboothttpsexample;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by FrancescoXX on 17/05/2018.
+ */
+
+@RestController
+@RequestMapping("/hello")
+public class HelloResource {
+
+    @GetMapping
+    public String hello() {
+        return "hello youtube";
+    }
+
+}
