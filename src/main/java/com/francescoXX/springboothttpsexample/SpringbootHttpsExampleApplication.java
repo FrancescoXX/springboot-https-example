1ef44998f73c32cf9d997d5cd6f1ee646278217c
Francesco Ciulla <francescociullaroma@gmail.com>
2018-05-17 02:17:12 +0200

    initial commit

---
+++ src/main/java/com/francescoXX/springboothttpsexample/SpringbootHttpsExampleApplication.java
@@ -0,0 +1,12 @@
+package com.francescoXX.springboothttpsexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHttpsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootHttpsExampleApplication.class, args);
+	}
+}
---
+++ src/test/java/com/francescoXX/springboothttpsexample/SpringbootHttpsExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.francescoXX.springboothttpsexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootHttpsExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
